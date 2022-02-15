d7944ef71b2cd735e7625965312897e3a04bb24a
Robert Shih <robertshih@google.com>
2022-02-14 16:02:55 -0800

    sm6150-common: Enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: Iaf321aabf7f0ef3d86c518982781d95c4a648506
    Signed-off-by: cykeek <cykeek@proton.me>
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -134,7 +134,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 # Fingerprint
