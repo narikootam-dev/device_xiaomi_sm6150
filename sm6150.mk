9ca8339f8af408c090e52cf58eae7862d98e95ab
basamaryan <basam.aryan@gmail.com>
2023-01-12 00:06:06 +0000

    sm6150-common: Build DRM 1.4 to vendor
    Change-Id: I1b642a1b0bc032f1e7f174bd072a485a20533fba
    Signed-off-by: cykeek <cykeek@proton.me>
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -134,7 +134,8 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
