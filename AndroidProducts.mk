dhimanparas20/android_device_realme_spartan

spartan: rmx3371 > spartan

AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_rmx3371.mk
+    $(LOCAL_DIR)/derp_spartan.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_rmx3371-user \
-    derp_rmx3371-userdebug \
-    derp_rmx3371-eng
+    derp_spartan-user \
+    derp_spartan-userdebug \
+    derp_spartan-eng

narikootam-dev/device_xiaomi_sm6150

AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rmx3371.mk
+    $(LOCAL_DIR)/lineage_spartan.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rmx3371-user \
-    lineage_rmx3371-userdebug \
-    lineage_rmx3371-eng
+    lineage_spartan-user \
+    lineage_spartan-userdebug \
+    lineage_spartan-eng